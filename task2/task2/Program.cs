06d25d1198cb8a7d561429551497508ca2f9910d
AsgarMadadli <asgarmm@code.edu.az>
2023-07-17 01:20:14 -0700

    completed

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace task2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<int> numbers = new List<int> { -3, 5, -7, 10, 0, 15, 7, -1, 11, -9, 2 };
+
+        var positiveNumbersQuery = from num in numbers
+                                   where num > 0 && num <= 11
+                                   select num;
+
+        List<int> positiveNumbers = positiveNumbersQuery.ToList();
+
+        Console.Write("The numbers within the range of 1 to 11 are: ");
+        foreach (int num in positiveNumbers)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(num + " ");
         }
     }
 }
