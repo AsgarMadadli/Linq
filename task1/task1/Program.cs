96bacb4437195be6aa5aef95a5e1880bdfb3b481
AsgarMadadli <asgarmm@code.edu.az>
2023-07-17 01:17:27 -0700

    completed

--- task1/task1/Program.cs
+++ task1/task1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace task1
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<int> numbers = Enumerable.Range(0, 10).ToList();
+
+        var evenNumbersQuery = from num in numbers
+                               where num % 2 == 0
+                               select num;
+
+        List<int> evenNumbers = evenNumbersQuery.ToList();
+
+        Console.WriteLine("The numbers which produce the remainder 0 after divided by 2 are:");
+        foreach (int num in evenNumbers)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(num + " ");
         }
     }
 }
